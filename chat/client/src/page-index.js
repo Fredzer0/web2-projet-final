5a49a3cd9b615f180bc0ec1f7cf0fdc55b44353f
fredzer0 <dinoqc@gmail.com>
2021-11-29 12:33:16 -0500

    weee

--- chat/client/src/page-index.js
+++ chat/client/src/page-index.js
@@ -5,3 +5,27 @@ window.addEventListener("load", () => {
         return signin(this);
     }
 });
+
+const flecheGauche = () => {
+    character1 = document.getElementById('background-character1');
+    character2 = document.getElementById('select-character');
+    character3 = document.getElementById('background-character2');
+
+
+}
+
+const flecheDroite = () => {
+    character1 = document.getElementById('background-character1');
+    character2 = document.getElementById('select-character');
+    character3 = document.getElementById('background-character2');
+
+}
+
+
+//append/remove child
+//local storage
+stockage = localStorage
+
+var background = stockage.setItem(document.getElementById("background-list"));
+var selectedCharacter = stockage.setItem("");
+
