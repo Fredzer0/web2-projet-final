30e2d9fa0bedb727f77c089073f01caf114ba170
Fred <dinoqc@gmail.com>
2021-12-04 22:53:08 -0500

    f

--- chat/client/src/page-index.js
+++ chat/client/src/page-index.js
@@ -1,6 +1,6 @@
 
-import {signin} from './chat-api' 
-import { characterList } from "../page-chat"
+//import {signin} from './chat-api' 
+//import { characterList } from "./page-chat"
 
 
 window.addEventListener("load", () => {
@@ -18,8 +18,7 @@ const flecheGauche = () => {
     var character2 = document.getElementById('select-character');
     var character3 = document.getElementById('background-character2');
 
-    character1.remove();
-    
+    character1.replaceWith(character2);
 
 }
 
@@ -38,3 +37,67 @@ stockage = localStorage
 var background = stockage.setItem(document.getElementById("background-list"));
 var selectedCharacter = stockage.setItem("");
 
+
+let characterList = [
+    {  
+        id: id++,
+        name: "Roland",
+        path: "img/sprites/RolandSprite.png",
+        cardPath: "img/cards/rolandCard.png",
+    },
+    {   
+        id: id++,
+        name: "Malkuth",
+        path: "img/sprites/MalkuthSprite.png",
+        cardPath: "img/cards/malkuthCard.png",
+    },
+    {   
+        id: id++,
+        name: "Yesod",
+        path: "img/sprites/YesodSprite.png",
+        cardPath: "img/cards/yesodCard.png",
+    },
+    {   
+        id: id++,
+        name: "Netzach",
+        path: "img/sprites/NetzachSprite.png",
+        cardPath: "img/cards/netzachCard.png",
+    },
+    {   
+        id: id++,
+        name: "Hod",
+        path: "img/sprites/HodSprite.png",
+        cardPath: "img/cards/hodCard.png",
+    },
+    {   
+        id: id++,
+        name: "Tipereth",
+        path: "img/sprites/TiperethSprite.png",
+        cardPath: "img/cards/tiperethCard.png",
+    },
+    {   
+        id: id++,
+        name: "Gebura",
+        path: "img/sprites/GeburaSprite.png",
+        cardPath: "img/cards/geburaCard.png",
+    },
+    {   
+        id: id++,
+        name: "Chesed",
+        path: "img/sprites/ChesedSprite.png",
+        cardPath: "img/cards/chesedCard.png",
+    },
+    {   
+        id: id++,
+        name: "Binah",
+        path: "img/sprites/BinahSprite.png",
+        cardPath: "img/cards/binahCard.png",
+    },
+    {   
+        id: id++,
+        name: "Hokma",
+        path: "img/sprites/HokmaSprite.png",
+        cardPath: "img/cards/hokmaCard.png",
+    },
+
+]
